0c3d328de0bac9e67c47dde0fdb1f1b2b0f12848
P.Sangeetha <147184589+Sangeetha-0502@users.noreply.github.com>
2024-08-08 07:50:46 +0530

    Update Bubble_Sort

--- bubble.java
+++ bubble.java
@@ -4,18 +4,12 @@ public class bubble{
 		int nums[]= {6,5,3,9,1,2};
 		int temp=0;
 		int n=nums.length;
-		
-		
-		
 		for(int i=0;i<n-1;i++) {
-			 
-				if(nums[i]>nums[i+1]) {
-					temp=nums[i];
-					nums[i]=nums[i+1];
-					nums[i+1]=temp;
-					
-					}		
-			
+		   if(nums[i]>nums[i+1]) {
+			temp=nums[i];
+			nums[i]=nums[i+1];
+			nums[i+1]=temp;
+		   }		
 		}
 		for(int num:nums) {
 			System.out.print(num +" ");
